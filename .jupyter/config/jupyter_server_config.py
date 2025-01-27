2623eae972a43f871e6404b5cc8ef30b84be09c1
Paul Romer <paul@paulromer.net>
2025-01-27 17:55:11 -0500

    clean up

--- .jupyter/config/jupyter_server_config.py
+++
@@ -1,9 +0,0 @@
-# Configuration file for jupyter-server.
-
-c = get_config()  #noqa
-
-c.ServerApp.terminado_settings = {
-'shell_command': 'bash --rcfile .jupyter/.bashrc'
-}
-
-c.ContentsManager.allow_hidden = True
